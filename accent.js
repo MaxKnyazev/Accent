218423b227050d40e2f9d7e2c77390808ad9ade3
MaxKnyazev <44132686+MaxKnyazev@users.noreply.github.com>
2018-12-21 22:11:40 +0300

    add files
    add files

---
+++ accent.js
@@ -0,0 +1,49 @@
+let words = [
+    {trueWord : 'клАла', falseWord : 'клалА'},
+    {trueWord : 'звонИт', falseWord : 'звОнит'},
+    {trueWord : 'свЕкла', falseWord : 'свеклА'},
+    {trueWord : 'слИвовый', falseWord : 'сливОвый'},
+    {trueWord : 'апострОф', falseWord : 'апОстроф'},
+    ];
+
+// let index = 4;
+// let word = 'апострОф';
+let index = 2;
+let word = 'свЕкла';
+let lengthWords = words.length-1;
+
+function replaceElements(arr, i) {
+    let temp = arr[i];
+    arr[i] = arr[lengthWords];
+    arr[lengthWords] = temp;
+}
+
+function checkWord(arr, i, wrd) {
+    if (wrd === arr[i].trueWord) {
+        if (i === lengthWords) {
+            lengthWords -= 1;
+        } else {
+            replaceElements(arr, i);
+            lengthWords -= 1;
+        }
+    }
+}
+
+// TODO: тестовая функция
+function _writeConsole() {
+    words.forEach(function(elem) {
+        console.log(elem);
+    });
+    console.log(lengthWords);
+}
+
+checkWord(words, index, word);
+
+_writeConsole();
+
+index = 2;
+word = 'апострОф';
+
+checkWord(words, index, word);
+
+_writeConsole();
